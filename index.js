9dfbcbc475779ad1f537c6a73a5e4a6fcf32b366
jagbegbo@gmail.com <jagbegbo@gmail.com>
2022-07-11 15:07:10 +0000

    country post successfull

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const Country = require("./src/models/country_schema");
 require("./src/database/connection");
+const {Router} = require("express");
+const { listeners } = require("./src/models/country_schema");
 
+//const router = Router();
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded());
 
 const port = 3000;
 
@@ -17,10 +22,13 @@ app.get('/', (request, response) => {
 })
 
 
-app.post("/country/save", async(request, response) => {
-    // const { code, name, phoneCode } = request.body;
-    // const newCountry = await Country.create({ code, name, phoneCode });
-    // const saveCountry = newCountry.save();
-    console.log(request.body);
-    response.send(request.body);
+app.post('/country/save',async(request, response) => {
+    try{
+        const { code, name, phoneCode } = request.body;
+        const newCountry = await Country.create({ code, name, phoneCode });
+        const saveCountry = newCountry.save();
+        console.log('saved succesfully');
+    }catch(err){
+        console.log(err)
+    }
 })
