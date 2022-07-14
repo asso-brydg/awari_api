c4b459c0be41b308676f23b49124e19d17a70712
charlesdzadu <dkccharlie@gmail.com>
2022-07-14 12:58:55 +0000

    Firebase integration

---
+++ functions/index.js
@@ -0,0 +1,34 @@
+const functions = require("firebase-functions");
+require("./src/database/connection");
+
+
+const express = require("express")
+const cors = require("cors")
+
+
+const api = express();
+
+api.use(express.json());
+api.use(express.urlencoded());
+api.use(cors());
+
+
+api.use(require("./src/routers/routes"))
+
+
+api.get("/test", (request, response)=>{
+    response.send("On va voir !!!!!!!!!!!!!!!!!!!!!");
+});
+
+
+
+
+
+
+const port = 3000;
+api.listen(port, () => console.log("server runing"));
+
+
+
+
+exports.api = functions.https.onRequest(api);
