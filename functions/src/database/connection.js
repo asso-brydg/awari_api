9401c8a7ff192af4ef0495b9f327a70762d6c108
charlesdzadu <dkccharlie@gmail.com>
2022-07-18 16:28:58 +0000

    env configuration

--- functions/src/database/connection.js
+++ functions/src/database/connection.js
@@ -3,6 +3,8 @@ const constants = require("../constants/constants")
 
 
 const uri = `mongodb+srv://awari_db_2022:${constants["MONGO_DB_PASSWORD"]}@awari.rpe58we.mongodb.net/awari?retryWrites=true&w=majority`;
+
+
 mongoose.connect(uri).then((value) => {
     console.log("Database is connected")
 }).catch((error) => {
